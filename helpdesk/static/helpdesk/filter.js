51ba6b79178da057d827aaa47e8f05ec055f2256
Stefano Brentegani <sbrentegani@gmail.com>
2014-07-31 23:17:52 +0200

    filter.js: apply/remove filterBoxShow class instead for sliding down/up

--- helpdesk/static/helpdesk/filter.js
+++ helpdesk/static/helpdesk/filter.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
     $("#filterBuilderButton").click(function() {
-        var boxName = "#filterBox" + $("#filterBuilderSelect").val();
-        $(boxName).slideDown();
+        var $boxName = $("#filterBox" + $("#filterBuilderSelect").val());
+        $boxName.addClass("filterBoxShow");
         return false;
     });
     $(".filterBuilderRemove").click(function() {
-        var boxName = "#" + $(this).parents(".filterBox").attr('id');
-        $(boxName).slideUp();
-        $(boxName).children("input:text").each(function() {
+        var $boxName = $("#" + $(this).parents(".filterBox").attr('id'));
+        $boxName.slideUp().removeClass("filterBoxShow");
+        $boxName.children("input:text").each(function() {
             $(this).val("");
         });
-        $(boxName).children("input:checkbox").each(function() {
+        $boxName.children("input:checkbox").each(function() {
             this.checked = false;
         });
-        $(boxName).children("select").each(function() {
+        $boxName.children("select").each(function() {
             this.selectedIndex = -1;
         });
         return false;
